e14176e9036ecfc584c8247ede2f1815f5733bb3
Juan Francisco Cardona McCormick <fcardona@eafit.edu.co>
2026-02-06 07:42:45 -0500

    Actualizando y corrigiendo el programa mostrar_poema.c windows

--- llamadas-al-sistema/windows/mostrar_poema.c
+++ llamadas-al-sistema/windows/mostrar_poema.c
@@ -5,38 +5,44 @@
 
 int
 main() {
-  HFILE hFile;
+  HANDLE hInFile;
   char buffer[SIZE_BUFFER + 1];
   
   ZeroMemory(buffer, SIZE_BUFFER);
   
-  hFile = OpenFile("../data/lope-de-vega-poema.txt", O_RDONLY);
+  hInFile = CreateFileA("..\\data\\lope-de-vega-poema.txt",
+			GENERIC_READ,
+			FILE_SHARE_READ,
+			NULL,
+			OPEN_EXISTING,
+			FILE_ATTRIBUTE_NORMAL,
+			NULL);
 
-  if (fd == -1) {
-    fprintf(stderr, "Error abriendo archivo: %d\n", errno);
-    _exit(1);
+  if (hInFile == INVALID_HANDLE_VALUE) {
+    fprintf(stderr, "Error abriendo archivo: %d\n", GetLastError());
+    ExitProcess(1);
   }
 
-  int tam;
-  tam = read(fd, buffer, SIZE_BUFFER);
-
-  if (tam == -1) {
-    fprintf(stderr, "Error leyendo archivo: %d\n", errno);
-    _exit(2);
+  DWORD tam;
+  if (!ReadFile(hInFile, buffer, SIZE_BUFFER, &tam, NULL)) {
+    fprintf(stderr, "Error leyendo archivo: %d\n", GetLastError());
+    ExitProcess(2);
   }
 
   buffer[tam] = '\0';
+  HANDLE hOutputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+  
   while (tam != 0) {
-    write(STDOUT_FILENO, buffer, tam);
-    tam = read(fd, buffer, SIZE_BUFFER);
-
-    if (tam == -1) {
+    WriteFile(hOutputHandle, buffer, tam, NULL, NULL);
+    if (!ReadFile(hInFile, buffer, SIZE_BUFFER, &tam, NULL)) {
       fprintf(stderr, "Error leyendo archivo: %d\n", errno);
-      _exit(3);
+      ExitProcess(3);
     }
+    
     buffer[tam] = '\0';
   }
 
-  close(fd); // Cierra el archivo que se esta leyendo.
+  CloseHandle(hOutputHandle); // Cierra el archivo que se esta escribiendo
+  CloseHandle(hInFile);
   _exit(0); // Termina 
 }
